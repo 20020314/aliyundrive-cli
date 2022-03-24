6d6c903315dc3d34f30e2d6c940f6d794503cbb9
zf1976 <1596863112@qq.com>
2022-03-24 14:59:53 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use std::collections::HashMap;
+use qrcode_term::qr;
 
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let resp = reqwest::blocking::get("https://httpbin.org/ip")?
         .json::<HashMap<String, String>>()?;
     println!("{:#?}", resp);
+    qr::print_qr("https://httpbin.org/ip").unwrap();
     Ok(())
 }
