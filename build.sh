6bdd6c494e5b6081c304776c261cf2c43cbdff96
remake <verticle@foxmail.com>
2022-05-28 23:36:35 +0800

    update

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+#alias rust-aarch64-musl='docker run --rm -it -v "$(pwd)":/home/rust/src messense/rust-musl-cross:aarch64-musl'
+#rust-aarch64-musl cargo build --release
---
+++ docker.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# docker build
+docker pull messense/rust-musl-cross:aarch64-musl
+
+docker pull messense/rust-musl-cross:arm-musleabi
+
+docker pull messense/rust-musl-cross:arm-musleabihf
+
+docker pull messense/rust-musl-cross:armv5te-musleabi
+
+docker pull messense/rust-musl-cross:armv7-musleabi
+
+docker pull messense/rust-musl-cross:armv7-musleabihf
+
+docker pull messense/rust-musl-cross:i586-musl
+
+docker pull messense/rust-musl-cross:i686-musl
+
+docker pull messense/rust-musl-cross:mips-musl
+
+docker pull messense/rust-musl-cross:mipsel-musl
+
+docker pull messense/rust-musl-cross:mips64-muslabi64
+
+docker pull messense/rust-musl-cross:mips64el-muslabi64
+
+docker pull messense/rust-musl-cross:powerpc64le-musl
+
+docker pull messense/rust-musl-cross:x86_64-musl
