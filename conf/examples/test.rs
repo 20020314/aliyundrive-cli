3372bb716c6348dc7a8eb7f1e892bfdeaba7259c
remake <verticle@foxmail.com>
2022-05-30 14:48:28 +0800

    update

--- conf/examples/test.rs
+++ conf/examples/test.rs
@@ -1,44 +1,27 @@
 use conf::rw::RW;
-use conf::Context;
-use drive::models::auth::{AuthorizationCode, Token};
-use drive::models::query::QueryQrCodeCkForm;
-use drive::models::{AuthorizationToken, Ok};
-use drive::scan::qr::LoginQrCodeScanner;
-use drive::scan::{QrCodeScanner, QrCodeScannerState};
-use std::{thread, time};
+use conf::{Config, Context};
 
 fn main() {
-    let scanner = LoginQrCodeScanner::new();
-    let generator_result = scanner.generator().unwrap();
-    qrcode::qr_print(generator_result.get_content()).expect("print qrcode error.");
-    let ck_form: QueryQrCodeCkForm = generator_result.into();
-    println!("{:#?}", &ck_form);
-    loop {
-        let query_result = scanner.query(&ck_form).unwrap();
-        if query_result.ok() {
-            if query_result.is_expired() {
-                break;
-            }
-            if query_result.is_confirmed() {
-                let mobile_login_result = query_result.get_mobile_login_result().unwrap();
-                let mobile_access_token = mobile_login_result.access_token().unwrap();
-                let mobile_refresh_token = mobile_login_result.refresh_token().unwrap();
-                let goto_result = scanner
-                    .token_login(Token::from(&mobile_access_token))
-                    .unwrap();
-                let web_login_result = scanner
-                    .get_token(AuthorizationCode::from(&goto_result))
-                    .unwrap();
-                let web_access_token = web_login_result.access_token().unwrap();
-                let web_refresh_token = web_login_result.refresh_token().unwrap();
-                let p1 = conf::AuthorizationToken::new(mobile_access_token, mobile_refresh_token);
-                let p2 = conf::AuthorizationToken::new(web_access_token, web_refresh_token);
-                let config = conf::Config::new(Some(p1), Some(p2));
-                Context::write(config).unwrap();
-                let c = Context::read().unwrap();
-                println!("{:#?}", c);
-            }
-        }
-        thread::sleep(time::Duration::from_secs(2));
-    }
+    let read_config = Context::read().unwrap();
+    println!("{:?}", read_config);
+
+    let p1 = conf::AuthorizationToken::new(String::from("a1"), String::from("a2"));
+    let p2 = conf::AuthorizationToken::new(String::from("a3"), String::from("a4"));
+    let config = Config::new(Some(p1), Some(p2));
+    crate::Context::write(config).unwrap();
+    let read_config = Context::read().unwrap();
+    println!("{:?}", read_config);
+
+    let t1 = Context::read_token(false).unwrap();
+    println!("{:?}", t1);
+    let t2 = Context::read_token(true).unwrap();
+    println!("{:?}", t2);
+
+    let p3 = conf::AuthorizationToken::new(String::from("a5"), String::from("a6"));
+    let p4 = conf::AuthorizationToken::new(String::from("a7"), String::from("a8"));
+    Context::write_token(false, p3).unwrap();
+    Context::write_token(true, p4).unwrap();
+
+    let read_config = crate::Context::read().unwrap();
+    println!("{:?}", read_config);
 }
